37a6854f7999fa769fde38aa4cc58e3cb6426a0a
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2021-12-26 18:48:45 +0000

    Added new file into featureB branch line

--- src/test/java/FeatureA_work/featureB_02.java
+++ src/test/java/FeatureA_work/featureB_02.java
@@ -3,5 +3,6 @@ package FeatureA_work;
 public class featureB_02 {
     public static void main(String[] args) {
         System.out.println("This is from new Branch featureB");
+        System.out.println("Temur akada ishlamayapti");
     }
 }
